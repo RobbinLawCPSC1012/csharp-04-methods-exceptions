4b89b01aa6b061b1f0dd2f0296ffd38efeb4832d
Robbin Law <robbinl@telusplanet.net>
2019-10-07 11:07:05 -0600

    a

---
+++ 40-try-catch-Triangles/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _40_try_catch_Triangles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
