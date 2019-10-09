962d4c8c0f89a5e238af1daf40c5e0dab3ce555d
Robbin <robbinl@telusplanet.net>
2019-10-09 07:43:25 -0600

    a

--- 02-methods-Demo/Program.cs
+++ 02-methods-Demo/Program.cs
@@ -8,16 +8,16 @@ namespace _02_methods_Demo
 {
     class Program
     {
-        static void printHello(){
+        static void printHello(){  // simplest method with no parameters and no return
             Console.WriteLine($"Hello");
         }
         
-        static void printIt(String myString, int myInt){
+        static void printIt(String myString, int myInt){ // intermediate method with parameters but no return
             Console.WriteLine($"Hey my string is: {myString}, and my int is {myInt}");
         }
 
 
-        static int add(int n1, int n2){
+        static int add(int n1, int n2){ // most complicated method with parameters and a return value
             return n1 + n2;
         }
 
