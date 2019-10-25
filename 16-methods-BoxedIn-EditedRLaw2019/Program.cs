5431ef6232773e8c43b4a516deb3fd51ea59e20a
RobbinL <RobbinL@WB304-99>
2019-10-25 08:54:09 -0600

    a

---
+++ 16-methods-BoxedIn-EditedRLaw2019/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_methods_BoxedIn_EditedRLaw2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = GetNumber("Enter the number of rows between 1 and 10: ");
+            char c = GetCharacter("Enter a character to print: ");
+            Console.WriteLine("\nDefault");
+            DrawTriangle(num);
+            Console.WriteLine("\nCustom");
+            DrawTriangle(num, c);
+        }
+
+        static int GetNumber(string msg)
+        {
+            bool invalidInput = true;
+            int num = 0;
+
+            while (invalidInput)
+            {
+                Console.Write(msg);
+                num = int.Parse(Console.ReadLine());
+                if (num > 0 && num <= 10)
+                    invalidInput = false;
+                else
+                    Console.WriteLine("Invalid Input. Number must be bigger than zero.");
+            }
+            return num;
+        }
+
+        static char GetCharacter(string msg)
+        {
+            bool validInput = false;
+            char c = 'c';
+
+            while (!validInput)
+            {
+                Console.Write(msg);
+                c = char.Parse(Console.ReadLine());
+                validInput = true;
+            }
+            return c;
+        }
+
+        static void DrawTriangle(int rows)
+        {
+            char c = '*';
+            DrawTriangle(rows, c);
+        }
+
+        static void DrawTriangle(int rows, char drawChar)
+        {
+            for (int i = 1; i <= rows; i++)
+            {
+                DrawRow(i, drawChar);
+            }
+        }
+
+        static void DrawRow(int len, char c)
+        {
+            for (int i = 0; i < len; i++)
+            {
+                Console.Write(c);
+            }
+            Console.Write("\n");
+        }
+    }
+}
